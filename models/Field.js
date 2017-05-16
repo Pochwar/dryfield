62d78e1edfd1a027a0da65001a13b9feaff312f1
gabriel <pour.blaireaux@yahoo.fr>
2017-05-16 15:44:47 +0200

    corrected Field.js

--- models/Field.js
+++ models/Field.js
@@ -1,10 +1,8 @@
 function Field(number, initialWaterReserve) {
     EventEmitter.call(this)
     this.number = number;
-    this.consumption;
     this.waterReserve = initialWaterReserve;
     this.maturity = false;
-    
 }
 
 Field.prototype = Object.create(EventEmitter.prototype);
@@ -12,14 +10,9 @@ Field.prototype.constructor = Field;
     
 
 
-Field.prototype.setWaterReserve = function() {
-   this.waterReserve -= this.consumption;
-   this.emit('set-waterReserve') {waterReserve : this.waterReserve}
-    }
-
-Field.prototype.setConsumption = function () {
-   /* this.consumption = ;*/
-    this.emit('set-consumption') {consumption : this.consumption}
+Field.prototype.setWaterReserve = function(quantity) {
+    this.waterReserve = quantity;
+    this.emit('set-waterReserve', {waterReserve : this.waterReserve});
 }
 
 Field.prototype.setMaturity = function(bool) {
