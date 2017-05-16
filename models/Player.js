0a6c8f0e50ef1c6b6d95fe3cc36292a32e95a0d6
Benoît Ripoche <ripoche.b@gmail.com>
2017-05-16 14:58:02 +0200

    add Player model

---
+++ models/Player.js
@@ -0,0 +1,38 @@
+function Player(initialMoney, initialWater, waterPrice, harvestPrice){
+    EventEmitter.call(this);
+
+    this.name;
+    this.nbHarvest = 0;
+    this.money = initialMoney;
+    this.water = initialWater;
+    this.waterPrice = waterPrice;
+    this.harvestPrice = harvestPrice;
+}
+
+Player.prototype = Object.create(EventEmitter.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.setNbHarvest = function(nbHarvest){
+    this.nbHarvest = nbHarvest;
+    this.emit("set-money", {money : this.money});
+}
+
+Player.prototype.setMoney = function(qtyMoney){
+    this.money = qtyMoney;
+    this.emit("set-money", {money : this.money});
+}
+
+Player.prototype.setWater = function(qtyWater){
+    this.water = qtyWater;
+    this.emit("set-water", {water : this.water});
+}
+
+Player.prototype.setWaterPrice = function(price){
+    this.waterPrice = price;
+    this.emit("set-water-price", {waterPrice : this.waterPrice});
+}
+
+Player.prototype.setHarvestPrice = function(price){
+    this.harvestPrice = price;
+    this.emit("set-harvest-price", {harvestPrice : this.harvestPrice});
+}
